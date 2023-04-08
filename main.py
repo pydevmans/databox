92a155d743ca790fa3877aa3173c53ef9509b7ae
Manan Bhavsar <mananbh9@live.com>
2023-04-08 13:06:11 -0400

    insert method of the Table tested.

--- main.py
+++ main.py
@@ -4,81 +4,62 @@ from collections import namedtuple
 from functools import reduce
 from utility.helpers import random_user_generator
 
+
 class InvalidPropException(Exception):
-	pass
+    pass
 
-class User:
-	def __init__(self, name, password, email):
-		self.pk = pk
-		self.name = name
-		self.last_name = last_name
-		self.password = password
-		self.email = email
-		self.address = address
-		self.telephone = telephone
-		self.phone = phone
-
-	def __str__(self):
-		return f"User: {self.name} {self.last_name}."
-
-	@staticmethod
-	def from_dict(data):
-		return User(**data)
 
 class Table:
-	def __init__(self, tablename, columns, joiner="|"):
-		"""
-		Title/Records are passed as tuple.
-		This table represent for any shape or size file database representation of the SQL type data.
-		"""
-		self.tablename = tablename
-		self.columns = columns
-		self.last_pk = 0
-		self.joiner = joiner
-		self.filelocation = "database/" + self.tablename + ".txt"
-		logging.debug(f"""
+    def __init__(self, tablename, columns, joiner="|"):
+        """
+        Title/Records are passed as tuple.
+        This table represent for any shape or size file database representation of the SQL type data.
+                Attributes:
+                tablename: string -> "Users"
+                columns: tuple(field_name:field_type) -> ("Name:str","Address:str")
+                joiner: string -> "|" or "," etc...
+        """
+        self.tablename = tablename
+        self.columns = columns
+        self.last_pk = 1
+        self.joiner = joiner
+        self.filelocation = "database/" + self.tablename + ".txt"
+        logging.debug(f"""
 			Table Creation of: {self.tablename}.
 				Columns added: {self.columns}.
 		""")
-		self._insert(*self.columns)
-		self._format(columns)
+        self._insert(*self.columns)
 
-	def __str__(self):
-		return f"Table: {self.tablename}."
+    def __str__(self):
+        return f"Table: {self.tablename}."
 
-	def __repr__(self):
-		return f"Table: {self.tablename}."
+    def __repr__(self):
+        return f"Table: {self.tablename}."
 
-	def _format(self, columns):
-		self.type_format = dict()
-		for col in columns:
-			name_and_type = col.split(":")
-			self.type_format.update({name_and_type[0]: name_and_type[1]})
+    def _insert(self, *args):
+        """
+        Exclusively for the Table creation purpose
+        """
+        with open(self.filelocation, mode="w") as file:
+            data = reduce(lambda x, y: x + self.joiner +
+                          str(y), args, "pk:int") + "\n"
+            file.write(data)
 
-	def _insert(self, *args):
-		"""
-		Exclusively for the Table creation purpose
-		"""
-		with open(self.filelocation, mode="w") as file:
-			data = reduce(lambda x, y: x + self.joiner + str(y), args, "pk:int") + "\n"
-			file.write(data)
-
-	def insert(self, *args, mode="a"):
-		with open(self.filelocation, mode=mode) as file:
-			data = str(self.last_pk)
-			data = reduce(lambda x, y: x+self.joiner+str(y), args, data) + "\n"
-			file.write(data)
-			self.last_pk += 1
+    def insert(self, **kwargs):
+        with open(self.filelocation, mode="a") as file:
+            data = str(self.last_pk)
+            data = reduce(lambda x, y: x+self.joiner+str(y),
+                          kwargs.values(), data) + "\n"
+            file.write(data)
+            self.last_pk += 1
 
 
 if __name__ == "__main__":
-	print("Done")
-	# url = input("Enter the path: ")
-	t = Table("TestTableUser", ("name:str","last_name:str","age:int","address:str","telephone:int","phone:int", "email:str") )
-	for i in range(5):
-		u = random_user_generator()
-		t.insert(u["first_name"], u["last_name"], u["age"], u["address"], u["telephone"], u["phone"], u["email"])
-
-	t.find("first_name", "Rajit Bhavsar")
-
-
+    print("Done")
+    # url = input("Enter the path: ")
+    t = Table("TestTableUser", ("first_name:str", "last_name:str", "age:int",
+              "address:str", "telephone:int", "phone:int", "email:str"))
+    for i in range(50):
+        u = random_user_generator()
+        t.insert(**u)
+    # t.find("first_name", "Rajit Bhavsar")
--- test_table.py
+++ test_table.py
@@ -8,22 +8,30 @@ from utility.helpers import random_user_generator
 class TestTable(unittest.TestCase):
 
     def setUp(self):
-        self.t = Table("TestTableUser", ("name:str", "last_name:str", "age:int",
+        self.t = Table("TestingTable", ("first_name:str", "last_name:str", "age:int",
                        "address:str", "telephone:int", "phone:int", "email:str"))
         self.user = random_user_generator()
 
     def tearDown(self):
         del self.t
+        del self.user
 
     def test_title(self):
-        title_on_test_data = "pk:int|name:str|last_name:str|age:int|address:str|telephone:int|phone:int|email:str\n"
+        title_on_test_data = "pk:int|first_name:str|last_name:str|age:int|address:str|telephone:int|phone:int|email:str\n"
         with open(self.t.filelocation, mode="r") as file:
             title_on_file = file.readline()
-            print("on File:", title_on_file)
-            print("on Test Data:", title_on_test_data)
             self.assertEqual(title_on_file, title_on_test_data)
 
     def test_insert(self):
-        u = self.user
-        self.t.insert(u.first_name, u.last_name, u.age,
-                      u.address, u.telephone, u.phone, u.email)
+        self.t.insert(**self.user)
+        dataset = (self.user["first_name"], self.user["last_name"], self.user["age"],
+                   self.user["address"], self.user["telephone"], self.user["phone"], self.user["email"])
+        title_on_test_data = f'1' + \
+            reduce(lambda a, b: a + self.t.joiner + str(b), dataset, "") + '\n'
+        with open(self.t.filelocation, mode="r") as file:
+            title_on_file = file.readlines()
+            self.assertEqual(title_on_file[-1], title_on_test_data)
+
+
+if __name__ == "__main__":
+    unittest.main()
