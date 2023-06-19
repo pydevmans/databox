d9b935326111b07386a513f6bf4d73088340f134
Manan Bhavsar <mananbh9@live.com>
2023-06-19 16:54:17 -0400

    ignore this one

--- downloadables/client.py
+++ downloadables/client.py
@@ -5,7 +5,7 @@ import requests
 TESTING = False  # Set False to test Prod Server
 
 if TESTING:
-    url = "http://localhost:5000"
+    url = "http://127.0.0.1:5000"
 else:
     url = "https://mb9.pythonanywhere.com"
 
