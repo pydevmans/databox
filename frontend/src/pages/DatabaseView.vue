9cf49eeecfeeda214e7c524551a447404f1be523
Manan Bhavsar <mananbh9@live.com>
2023-06-19 13:34:17 -0400

    updated the reqs for backend and added WIP files

---
+++ frontend/src/pages/DatabaseView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="features"></div>
+</template>
+
+<style scoped></style>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "FeatureView",
+  data() {
+    return {
+      data: "",
+    };
+  },
+  methods: {
+    fetchData() {
+      features().then((resp) => (this.data = resp.data));
+    },
+  },
+  created() {
+    this.fetchData();
+  },
+});
+</script>
